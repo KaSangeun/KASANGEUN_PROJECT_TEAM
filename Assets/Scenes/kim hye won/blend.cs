607cb3b439165828acad10fcd662a6102edd1ebb
heathnote <nononino13@naver.com>
2022-12-18 21:42:12 +0900

    ㅇㅇ

--- Assets/Scenes/kim hye won/blend.cs
+++ Assets/Scenes/kim hye won/blend.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class blend : MonoBehaviour
 {
+    Material myMaterial;
+    Shader myShader;
+    public Texture2D BlendTex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myShader = Shader.Find("My/PostEffects/Blend");
+        myMaterial = new Material(myShader);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        
+        myMaterial.SetTexture("_BlendTex", BlendTex);
+        Graphics.Blit(source, destination, myMaterial, 0);
     }
+
+    private void OnDisable()
+    {
+        if (myMaterial)
+        {
+            DestroyImmediate(myMaterial);
+        }
+    }
+
 }
